0e4b7a9eccf661dc3d47a72c5039250689af0f67
mohamed.Abdulaty <mohamed.Abdulaty@PI-IT-99>
2023-07-16 17:01:59 +0300

    Add project files.

---
+++ Repository/Repos/CompanyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Contracts.ICompany;
+using Entities.Models;
+using Repository.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repos
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext)
+        : base(repositoryContext)
+        {
+        }
+    }
+}
